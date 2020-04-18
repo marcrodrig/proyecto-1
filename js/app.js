f9ff0d7db4005d073705240f875f4d6a489c2edc
marcrodrig <marcelo.prf9@gmail.com>
2020-04-18 18:41:10 -0300

    Visualizácion (tabla y mapa) de algunos datos

--- js/app.js
+++ js/app.js
@@ -1,1 +1,134 @@
+let datos;
 
+function mostrar() {
+    var e = document.getElementById("botonMostrar");
+    var dia = e.options[e.selectedIndex].value;
+    //console.log(dia);
+    let json = "casos-" + dia + ".json";
+    //console.log(json);
+    $.getJSON(json, function (dato) {
+        console.log(dato);
+        datos = dato;
+        let res = document.querySelector("#res");
+        res.innerHTML = '';
+        for(let item of datos) {
+            res.innerHTML += `
+                <tr>
+                    <td>${item.provincia}</td>
+                    <td>${item.confirmados}</td>
+                    <td>${item.acumulados}</td>
+                </tr>
+            `
+        }
+        geojson.eachLayer(function (layer) {  
+              layer.setStyle({fillColor: getColor(layer.feature.properties.nam),
+                weight: 2,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3',
+                fillOpacity: 0.7});
+          });
+    
+      });
+    /* Otra forma de obtener el json:
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            datos = JSON.parse(this.responseText);
+            console.log(datos);
+            let res = document.querySelector("#res");
+            res.innerHTML = '';
+            for(let item of datos) {
+                res.innerHTML += `
+                    <tr>
+                        <td>${item.provincia}</td>
+                        <td>${item.confirmados}</td>
+                        <td>${item.acumulados}</td>
+                    </tr>
+                `
+            }
+            geojson.eachLayer(function (layer) {  
+                  layer.setStyle({fillColor: getColor(layer.feature.properties.nam),
+                    weight: 2,
+                    opacity: 1,
+                    color: 'white',
+                    dashArray: '3',
+                    fillOpacity: 0.7});
+              });
+        }
+    };   
+    xhttp.open('GET', url, true);
+    xhttp.send();*/
+    document.getElementById("tablaYmapa").style.visibility = "visible";
+}  
+
+function getColor(nombreProvincia) {
+    let d = getAcumuladosByProvincia(nombreProvincia);
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function getConfirmadosByProvincia(nombreProvincia) {
+    switch(nombreProvincia) {
+        case "Buenos Aires" : return datos[0].confirmados;
+        case "Ciudad Autónoma de Buenos Aires": return datos[1].confirmados;
+        case "Catamarca" : return datos[2].confirmados;
+        case "Chaco" : return datos[3].confirmados;
+        case "Chubut" : return datos[4].confirmados;
+        case "Córdoba" : return datos[5].confirmados;
+        case "Corrientes" : return datos[6].confirmados;
+        case "Entre Ríos" : return datos[7].confirmados;
+        case "Formosa" : return datos[8].confirmados;
+        case "Jujuy" : return datos[9].confirmados;
+        case "La Pampa" : return datos[10].confirmados;
+        case "La Rioja" : return datos[11].confirmados;
+        case "Mendoza" : return datos[12].confirmados;
+        case "Misiones" : return datos[13].confirmados;
+        case "Neuquén" : return datos[14].confirmados;
+        case "Río Negro" : return datos[15].confirmados;
+        case "Salta" : return datos[16].confirmados;
+        case "San Juan" : return datos[17].confirmados;
+        case "San Luis" : return datos[18].confirmados;
+        case "Santa Cruz" : return datos[19].confirmados;
+        case "Santa Fe" : return datos[20].confirmados;
+        case "Santiago del Estero" : return datos[21].confirmados;
+        case "Tierra del Fuego, Antártida e Islas del Atlántico Sur" : return datos[22].confirmados;
+        case "Tucumán" : return datos[23].confirmados;
+    } 
+}
+
+function getAcumuladosByProvincia(nombreProvincia) {
+    console.log(datos);
+    switch(nombreProvincia) {
+        case "Buenos Aires" : return datos[0].acumulados;
+        case "Ciudad Autónoma de Buenos Aires": return datos[1].acumulados;
+        case "Catamarca" : return datos[2].acumulados;
+        case "Chaco" : return datos[3].acumulados;
+        case "Chubut" : return datos[4].acumulados;
+        case "Córdoba" : return datos[5].acumulados;
+        case "Corrientes" : return datos[6].acumulados;
+        case "Entre Ríos" : return datos[7].acumulados;
+        case "Formosa" : return datos[8].acumulados;
+        case "Jujuy" : return datos[9].acumulados;
+        case "La Pampa" : return datos[10].acumulados;
+        case "La Rioja" : return datos[11].acumulados;
+        case "Mendoza" : return datos[12].acumulados;
+        case "Misiones" : return datos[13].acumulados;
+        case "Neuquén" : return datos[14].acumulados;
+        case "Río Negro" : return datos[15].acumulados;
+        case "Salta" : return datos[16].acumulados;
+        case "San Juan" : return datos[17].acumulados;
+        case "San Luis" : return datos[18].acumulados;
+        case "Santa Cruz" : return datos[19].acumulados;
+        case "Santa Fe" : return datos[20].acumulados;
+        case "Santiago del Estero" : return datos[21].acumulados;
+        case "Tierra del Fuego, Antártida e Islas del Atlántico Sur" : return datos[22].acumulados;
+        case "Tucumán" : return datos[23].acumulados;
+    }
+}
---
+++ js/mapa.js
@@ -0,0 +1,99 @@
+let map = L.map("map").setView([-39, -59], 4);
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var geojson = L.geoJson(provincias, { onEachFeature: onEachFeature }).addTo(map);
+
+function getColorByGrade(d) {
+    return  d > 1000 ? '#800026' :
+            d > 500 ? '#BD0026' :
+            d > 200 ? '#E31A1C' :
+            d > 100 ? '#FC4E2A' :
+            d > 50 ? '#FD8D3C' :
+            d > 20 ? '#FEB24C' :
+            d > 10 ? '#FED976' :
+                     '#FFEDA0';
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        fillColor: getColor(layer.feature.properties.nam),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    });
+
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<center><h4>Casos confirmados</h4>' + (props ?
+        '<b>' + props.nam + '</b><br />' + getConfirmadosByProvincia(props.nam)
+        + ' casos<br /><i>Acumulados: </i>' + getAcumuladosByProvincia(props.nam) + ' casos<br />'
+        + '</center>'
+        : 'Deslícese sobre una provincia');
+};
+
+info.addTo(map);
+
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColorByGrade(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
